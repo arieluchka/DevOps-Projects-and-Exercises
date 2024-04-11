5818c9e47282f5ed3de2ddf80cb66710a925e54c
Ariel <ariel.agra@gmail.com>
2024-04-11 18:50:23 +0300

    added majority exerccise

---
+++ Topics/02-Python/Exercises/1) Easy/4) Majority/README.md
@@ -0,0 +1,22 @@
+## Majority
+Given an array `nums` of size `n`, return the majority element.
+
+The majority element is the element that appears more than `⌊n / 2⌋` times. **You may assume that the majority element always exists in the array.**
+
+Write your code in the "majority" function. The function should return **an integer**.
+
+**Run the file main.py to test your code.**
+
+## Examples:
+### Example 1:
+**Input**: nums = [3,2,3]
+
+**Output**: 3
+
+### Example 2:
+**Input**: nums = [2,2,1,1,1,2,2]
+
+**Output**: 2
+
+
+---
---
+++ Topics/02-Python/Exercises/1) Easy/4) Majority/main.py
@@ -0,0 +1,32 @@
+from auto_test import *
+
+class Solution(object):
+# ------------------------Your Solution in here-----------------------------------------------
+    def majority(self, nums):
+
+
+
+
+    
+
+
+# --------------------------------------------------------------------------------------------
+#                   The function should return a boolean (True/False)
+
+
+def solution_testings():
+    _ = Solution()
+    for nums, answer in majority_list:
+        print(f"testing nums {nums} for majority")
+        res = _.majority(nums)
+        if res == answer:
+            print(f"function passed!")
+        else:
+            raise Exception(f"Your function returned {res}, but the result should be {answer}")
+    print("*"*25)
+    print("You Did it!")
+    print("*" * 25)
+
+if __name__ == '__main__':
+    solution_testings()
+
